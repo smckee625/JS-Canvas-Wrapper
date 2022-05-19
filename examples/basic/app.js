9e50b5c8e3a01a33609297094ea1fbc64f523922
Smckee625 <Smckee625@gmail.com>
2022-05-19 19:20:11 +0100

    Fixed basic example

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -1,4 +1,4 @@
-import { Canvas, Keyboard, Rectangle } from '../../Infinite.js';
+import { Canvas, Rectangle } from '../../Infinite.js';
 
 var canvas = new Canvas();
 canvas.colour = 'lightblue';
@@ -6,23 +6,10 @@ canvas.colour = 'lightblue';
 var square = new Rectangle(100,100);
 square.colour = 'black';
 
-var square1 = new Rectangle(100,100, 200,200);
-square1.colour = 'black';
-
 canvas.onUpdate(() =>
 {
-    if (Keyboard.isKeyDown('w')) square.y--;
-    if (Keyboard.isKeyDown('a')) square.x--;
-    if (Keyboard.isKeyDown('s')) square.y++;
-    if (Keyboard.isKeyDown('d')) square.x++;
-    if (Keyboard.isKeyDown('r')) square1.rotate(2)
-
-    if (square.intersects(square1)) square1.colour = 'red';
-    else square1.colour = 'black';
-
     canvas.clear();
     canvas.draw(square);
-    canvas.draw(square1);
 });
 
 canvas.run();
